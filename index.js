13811e4693f5a4d8ba8cde6ce80fc53dc09c2b3b
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-28 01:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {breakfast: 'eggs'}; 
 function updateObjectWithKeyAndValue (object,key,value) {'eggs','bacon', '2 cups'}
 
+recipes = {breakfast: 'eggs,cheese'}; 
 
 function updateObjectWithKeyAndValue (value, key, obkect) {'breakfast'} {'1 cup'} 
